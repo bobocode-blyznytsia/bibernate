9254df6060b4a20c35927f7fe084f8579cba4300
Oleh Haliak <aiupea5@Amway.com>
2023-03-23 14:19:14 +0200

    [issue-#8] Sync merge: Add exception package

---
+++ src/main/java/com/bobocode/blyznytsia/bibernate/exception/BibernateException.java
@@ -0,0 +1,11 @@
+package com.bobocode.blyznytsia.bibernate.exception;
+
+public class BibernateException extends RuntimeException {
+  public BibernateException(String message) {
+    super(message);
+  }
+
+  public BibernateException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
---
+++ src/main/java/com/bobocode/blyznytsia/bibernate/exception/MalformedEntityException.java
@@ -0,0 +1,7 @@
+package com.bobocode.blyznytsia.bibernate.exception;
+
+public class MalformedEntityException extends BibernateException {
+  public MalformedEntityException(String message) {
+    super(message);
+  }
+}
