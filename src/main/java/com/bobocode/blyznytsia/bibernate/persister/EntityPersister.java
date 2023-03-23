7c51748e6372980a4fb71754c177e9ac882eca4d
Oleh Haliak <aiupea5@Amway.com>
2023-03-23 14:53:34 +0200

    [issue-#8] Sync merge: Updated EntityPersister interface

--- src/main/java/com/bobocode/blyznytsia/bibernate/persister/EntityPersister.java
+++ src/main/java/com/bobocode/blyznytsia/bibernate/persister/EntityPersister.java
@@ -1,4 +1,19 @@
 package com.bobocode.blyznytsia.bibernate.persister;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Optional;
+
 public interface EntityPersister {
+  <T> Optional<T> findById(Class<T> entityType, Object id);
+
+  <T> Optional<T> findOneBy(Class<T> entityType, Field key, Object value);
+
+  <T> List<T> findAll(Class<T> entityType, Field key, Object value);
+
+  <T> T insert(T entity);
+
+  <T> T update(T entity);
+
+  <T> T delete(T entity);
 }
