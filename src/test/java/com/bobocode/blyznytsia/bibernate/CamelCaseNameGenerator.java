7915bcd39cd5aa1f690420b3155d922777d11921
Oleh Haliak <aiupea5@Amway.com>
2023-04-05 16:19:31 +0300

    [issue-22] Minor improvement to test method name generator

--- src/test/java/com/bobocode/blyznytsia/bibernate/CamelCaseNameGenerator.java
+++ src/test/java/com/bobocode/blyznytsia/bibernate/CamelCaseNameGenerator.java
@@ -6,6 +6,14 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 public class CamelCaseNameGenerator extends DisplayNameGenerator.Simple {
   @Override
   public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
-    return testMethod.getName().replaceAll("([a-z])([A-Z])","$1 $2").toLowerCase();
+    return capitalizeFirstWord(splitWords(testMethod.getName()));
+  }
+
+  String splitWords(String str) {
+    return str.replaceAll("([a-z])([A-Z])", "$1 $2").toLowerCase();
+  }
+
+  String capitalizeFirstWord(String str) {
+    return Character.toUpperCase(str.charAt(0)) + str.substring(1);
   }
 }
